e626b8e8550a7b9c58a2c182848969b919b5f91e
toyeade1 <96374465+toyeade1@users.noreply.github.com>
2022-09-15 09:42:38 -0400

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,6 @@
+print('Welcome to the tip calculator.')
+totalBill = float(input('What was the total bill? $'))
+billSplit = int(input('How many people to split the bill? '))
+tipPercentage = int(input('What percentage tip would you like to give? 10, 12, or 15? '))
+newAmount = totalBill + ((tipPercentage/100) * totalBill)
+print('Each person should pay: $',round(newAmount/billSplit,1))
